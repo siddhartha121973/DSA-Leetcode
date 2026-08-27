08daf78796903ca6d5dfd242bf27e859843e49aa
Siddhartha Gupta <siddhartha121973@gmail.com>
2026-08-27 21:18:09 +0530

    Time: 1 ms (98.54%) | Memory: 42.7 MB (23.58%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        long left = 1;
+        long right = x / 2;
+        int ans = 1;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+
+            if (mid * mid <= x) {
+                ans = (int) mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
